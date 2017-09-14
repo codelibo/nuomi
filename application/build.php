b3fe8e127fffd6cad03884a8f05c41513ad5a68b
sunshineliulu <liulurose@sina.com>
2017-09-14 18:16:57 +0800

    自动生成模块

---
+++ application/build.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liulu
+ * Date: 2017/9/14
+ * Time: 下午5:35
+ */
+return [
+    // 生成应用公共文件
+    '__file__' => ['common.php', 'config.php', 'database.php'],
+
+    // 定义demo模块的自动生成 （按照实际定义的文件名生成）
+    /*
+    'demo'     => [
+        '__file__'   => ['common.php'],
+        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
+        'controller' => ['Index', 'Test', 'UserType'],
+        'model'      => ['User', 'UserType'],
+        'view'       => ['index/index'],
+    ],
+    */
+    // 其他更多的模块定义
+    'admin' => [
+        '__dir__'    => ['controller', 'view'],
+        'controller' => ['Index'],
+        'view'       => ['index/index'],
+    ],
+
+    'bis'=>[
+        '__dir__'    => ['controller', 'model', 'view'],
+        'controller' => ['Register', 'Login'],
+    ],
+];
