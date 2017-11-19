248ed9a205cba28e6ca9937e12b791d33e1f30e5
liulu <liulurose@sina.com>
2017-11-19 23:50:19 +0800

    使用phpmailer类发送邮件

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -11,6 +11,8 @@ class Index extends Controller
 
     public function welcome()
     {
+        \phpmailer\Email::send("813937382@qq.com","title","phpmailer");
+//        return "发送邮件成功";
 //        return $this->fetch();
         return "欢迎来到o2o主后台首页！";
     }
---
+++ application/extra/email.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liulu
+ * Date: 2017/11/19
+ * Time: 18:24
+ * 发送邮件相关配置
+ */
+return [
+    'host' => 'smtp.163.com',
+    'port' => 25,
+    'username' => "xxx@163.com",
+    'password' => "xxx",
+];
